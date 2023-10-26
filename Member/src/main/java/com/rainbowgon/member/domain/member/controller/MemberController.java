1d555870ecfe1395f2d841664a9c793f443e0686
최해미 <goal961@naver.com>
2023-10-26 17:44:12 +0900

    [env] 서버 path 수정 (default /api)

--- Member/src/main/java/com/rainbowgon/member/domain/member/controller/MemberController.java
+++ Member/src/main/java/com/rainbowgon/member/domain/member/controller/MemberController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/member")
+@RequestMapping("/members")
 public class MemberController {
 
     private final MemberService memberService;
