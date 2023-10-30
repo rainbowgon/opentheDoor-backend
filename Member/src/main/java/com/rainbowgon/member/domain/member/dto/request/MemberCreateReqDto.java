4e426fbec961991111064f236aef9e89eb46f887
chogoal <goal961@naver.com>
2023-10-30 09:06:08 +0900

    [fix] 회원가입 시 받아올 정보 수정

---
+++ Member/src/main/java/com/rainbowgon/member/domain/member/dto/request/MemberCreateReqDto.java
@@ -0,0 +1,27 @@
+package com.rainbowgon.member.domain.member.dto.request;
+
+import com.rainbowgon.member.domain.member.entity.Provider;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberCreateReqDto {
+
+    private String name;
+    private String phoneNumber;
+    private Provider provider;
+    private String providerId;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate birthDate;
+
+    // Profile에 저장되는 정보
+    private String nickname;
+    private String profileImage;
+
+}
---
+++ Member/src/main/java/com/rainbowgon/member/domain/member/dto/response/MemberInfoResDto.java
@@ -0,0 +1,26 @@
+package com.rainbowgon.member.domain.member.dto.response;
+
+import com.rainbowgon.member.domain.member.entity.Member;
+import com.rainbowgon.member.domain.profile.entity.Profile;
+import lombok.*;
+
+import java.util.UUID;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class MemberInfoResDto { // 회원 정보가 담긴 응답 객체
+
+    private UUID memberId;
+    private String nickname;
+    private String profileImage;
+
+    public static MemberInfoResDto from(Member member, Profile profile) {
+        return MemberInfoResDto.builder()
+                .memberId(member.getId())
+                .nickname(profile.getNickname())
+                .profileImage(profile.getProfileImage())
+                .build();
+    }
+}
