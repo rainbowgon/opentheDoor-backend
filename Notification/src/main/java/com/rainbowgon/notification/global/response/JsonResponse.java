9a031c16fd619f36fcf7d854369dec3a479397a9
전혜련 <davidpoplar@naver.com>
2023-10-30 17:01:53 +0900

    [fix] 폴더 구조 변경

--- Notification/src/main/java/com/rainbowgon/notification/global/response/JsonResponse.java
+++ Notification/src/main/java/com/rainbowgon/notification/global/response/JsonResponse.java
@@ -1,4 +1,4 @@
-package com.rainbowgon.notification.domain.notification.response;
+package com.rainbowgon.notification.global.response;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
