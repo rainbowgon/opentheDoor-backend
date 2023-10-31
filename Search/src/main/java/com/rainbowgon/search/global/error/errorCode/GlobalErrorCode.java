491590ced16e267b29f574c069f349f5e26436f1
변정원 <labhg64@gmail.com>
2023-10-31 14:22:52 +0900

    [fix] 에러 핸들링 컨벤션

--- Search/src/main/java/com/rainbowgon/search/global/error/errorCode/GlobalErrorCode.java
+++ Search/src/main/java/com/rainbowgon/search/global/error/errorCode/GlobalErrorCode.java
@@ -19,7 +19,7 @@ public enum GlobalErrorCode implements BaseErrorCode {
 
     CUSTOM_INTERNAL_SERVER_ERROR(INTERNAL_SERVER_ERROR, "GLOBAL_500_1", "서버 오류. 관리자에게 문의 부탁드립니다."),
 
-    THEME_NOT_FOUND(NOT_FOUND, "THEME-001", "주어진 ID에 해당하는 테마가 없습니다.");
+    THEME_NOT_FOUND(NOT_FOUND, "THEME-404-1", "주어진 ID에 해당하는 테마가 없습니다.");
 
     private HttpStatus status;
     private String code;
