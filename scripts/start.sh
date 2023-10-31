d46a3be80784624632a41eee0ee2b4afb7fad598
Lee, Nam Gon <nglee.dev@gmail.com>
2023-10-31 17:29:00 +0900

    [feat] appspec에서 사용하는 scipts 추가

---
+++ scripts/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ROOT_PATH="/home/ubuntu/app"
+JAR="$ROOT_PATH/app.jar"
+
+# Spring 에러 코드
+CONTAINER="app_container"
+IMAGE="app_image"
+
+docker build -t "$IMAGE" .
+docker run -dp 8080:8080 --name "$CONTAINER" "$IMAGE"
