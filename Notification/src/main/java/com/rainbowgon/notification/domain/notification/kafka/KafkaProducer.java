1cfb783dffafb3b67132763055655d5dbd855f21
전혜련 <davidpoplar@naver.com>
2023-11-01 16:49:05 +0900

    [feat] Kafka Producer 코드 작성

--- Notification/src/main/java/com/rainbowgon/notification/domain/notification/kafka/KafkaProducer.java
+++ Notification/src/main/java/com/rainbowgon/notification/domain/notification/kafka/KafkaProducer.java
@@ -1,4 +1,30 @@
 package com.rainbowgon.notification.domain.notification.kafka;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
 public class KafkaProducer {
-}
+
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public void send(String topic, String testMessage){
+        log.info("kafka Prdoducer send testMessage : " + testMessage);
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonInString = "";
+
+        try{
+            jsonInString = mapper.writeValueAsString(testMessage);
+        }catch(JsonProcessingException e){
+            e.printStackTrace();
+        }
+
+        kafkaTemplate.send(topic,jsonInString);
+    }
+}~
--- Notification/src/main/java/com/rainbowgon/notification/domain/notification/kafka/KafkaProducerConfig.java
+++ Notification/src/main/java/com/rainbowgon/notification/domain/notification/kafka/KafkaProducerConfig.java
@@ -1,4 +1,39 @@
 package com.rainbowgon.notification.domain.notification.kafka;
 
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
 public class KafkaProducerConfig {
+
+    @Autowired
+    Environment env;
+
+    @Bean
+    public ProducerFactory<String, String> producerFactory(){
+        Map<String,Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, env.getProperty("localhost:9092"));
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate(){
+        return new KafkaTemplate<>(producerFactory());
+    }
+
 }
