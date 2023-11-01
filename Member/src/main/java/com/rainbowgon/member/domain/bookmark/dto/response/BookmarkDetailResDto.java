f479c50f81c23f01a8d2b51a4137d06543d18d52
최해미 <goal961@naver.com>
2023-11-01 16:02:42 +0900

    [feat] Bookmark 등록/해제, 조회, 알림 기능 구현

--- Member/src/main/java/com/rainbowgon/member/domain/bookmark/dto/response/BookmarkDetailResDto.java
+++ Member/src/main/java/com/rainbowgon/member/domain/bookmark/dto/response/BookmarkDetailResDto.java
@@ -8,12 +8,11 @@ import lombok.*;
 @Builder
 public class BookmarkDetailResDto { // 마이페이지에서 보여지는 북마크 리스트 객체
 
+    /* 테마 정보 */
+    private Long themeId; // 테마 ID
     private String poster; // 테마 포스터
     private String title; // 테마명
     private String venue; // 지점명
-    private Float rating; // 평균 별점
-    private Integer reviewCnt; // 리뷰 수(별점 수)
-    private Integer bookmarkCnt; // 북마크 수
     private String[] genre; // 장르
     private Integer maxHeadCnt; // 최대 인원 수
     private Integer minHeadCnt; // 최소 인원 수
@@ -24,5 +23,18 @@ public class BookmarkDetailResDto { // 마이페이지에서 보여지는 북마
     private Float lockRatio; // 장치 비율
     private Float horrorLevel; // 공포도
 
+    /* 리뷰 정보 */
+    private Integer reviewCnt; // 리뷰 수(별점 수)
+    private Float rating; // 평균 별점
+
+    /* 북마크 정보 */
+    private Long bookmarkId; // 북마크 ID
+    private Integer bookmarkCnt; // 북마크 수
+
+
+    public static BookmarkDetailResDto of() { // 테마 정보, 리뷰 정보, 북마크 정보
+        return BookmarkDetailResDto.builder()
+                .build();
+    }
 
 }
