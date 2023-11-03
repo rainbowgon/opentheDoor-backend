f8213dc00867a97502b3f55955962a8328cb899b
Byun Jung Won <101097111+Byungul@users.noreply.github.com>
2023-11-03 17:23:35 +0900

    [chore] Search 패키지 이름 변경 (#82)

--- Sender/src/main/java/com/rainbowgon/senderservice/domain/kafka/dto/in/MessageReqDTO.java
+++
@@ -1,6 +0,0 @@
-package com.rainbowgon.sender.domain.kafka.dto.in;
-
-
-public class MessageReqDTO {
-
-}
---
+++ search-service/src/main/java/com/rainbowgon/searchservice/global/config/EsClientConfig.java
@@ -0,0 +1,35 @@
+package com.rainbowgon.searchservice.global.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+import javax.validation.constraints.NotNull;
+
+@Configuration
+@EnableElasticsearchRepositories
+public class EsClientConfig extends ElasticsearchConfiguration {
+
+    @Value("${elasticsearch.host}")
+    private String host;
+
+    @Value("${elasticsearch.port}")
+    private String port;
+
+//    @Value("${elasticsearch.username}")
+//    private String username;
+//
+//    @Value("${elasticsearch.password}")
+//    private String password;
+
+    @NotNull
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedTo(host + ":" + port)
+//            .withBasicAuth(username, password)
+                .build();
+    }
+}
