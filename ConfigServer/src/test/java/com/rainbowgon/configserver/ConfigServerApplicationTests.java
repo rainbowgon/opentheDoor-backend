b8999abae16f5fb6efeb5f9db1e37d285054a719
Lee, Nam Gon <nglee.dev@gmail.com>
2023-11-03 14:22:27 +0900

    [env] Eureka 서버 생성 (#67)

---
+++ ConfigServer/src/test/java/com/rainbowgon/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rainbowgon.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ Eureka/src/test/java/com/rainbowgon/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rainbowgon.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
