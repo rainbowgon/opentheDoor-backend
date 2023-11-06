78ec9046b73f7ee6633f8bd53f95fa3b189a391a
이남곤 <nglee.dev@gmail.com>
2023-11-06 22:41:38 +0900

    [env] Application 등록

--- reservation-service/src/main/java/com/rainbowgon/reservationservice/ReservationServiceApplication.java
+++ reservation-service/src/main/java/com/rainbowgon/reservationservice/ReservationServiceApplication.java
@@ -2,8 +2,12 @@ package com.rainbowgon.reservationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class ReservationServiceApplication {
 
     public static void main(String[] args) {
