98a7d5e56e7612694d9e77974dc2efb3f9934e82
전혜련 <davidpoplar@naver.com>
2023-11-07 10:40:43 +0900

    [fix] rename directory

--- notification-service/src/main/java/com/rainbowgon/notificationservice/domain/kafka/dto/output/MessageOutDto.java
+++ notification-service/src/main/java/com/rainbowgon/notificationservice/domain/kafka/dto/output/MessageOutDto.java
@@ -1,9 +1,9 @@
-package com.rainbowgon.notificationservice.domain.kafka.dto.out;
+package com.rainbowgon.notificationservice.domain.kafka.dto.output;
 
 
-import com.rainbowgon.notificationservice.domain.notification.client.dto.in.BookmarkInDto;
-import com.rainbowgon.notificationservice.domain.notification.client.dto.in.ReservationInDto;
-import com.rainbowgon.notificationservice.domain.notification.client.dto.in.WaitingInDto;
+import com.rainbowgon.notificationservice.domain.notification.client.dto.input.BookmarkInDto;
+import com.rainbowgon.notificationservice.domain.notification.client.dto.input.ReservationInDto;
+import com.rainbowgon.notificationservice.domain.notification.client.dto.input.WaitingInDto;
 import com.rainbowgon.notificationservice.domain.notification.entity.NotificationType;
 import com.rainbowgon.notificationservice.domain.notification.entity.ViewStatus;
 import lombok.*;
