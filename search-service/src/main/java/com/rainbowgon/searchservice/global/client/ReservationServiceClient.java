3ed69d10b466560517d1883fe43f3844b944bdab
Byun Jung Won <101097111+Byungul@users.noreply.github.com>
2023-11-09 13:27:16 +0900

    [fix] 작은 에러 수정 (#188)

---
+++ search-service/src/main/java/com/rainbowgon/searchservice/global/client/ReservationServiceClient.java
@@ -0,0 +1,7 @@
+package com.rainbowgon.searchservice.global.client;
+
+//@FeignClient(name = "reservation-service")
+public interface ReservationServiceClient {
+
+//    @GetMapping("/reservations")
+}
