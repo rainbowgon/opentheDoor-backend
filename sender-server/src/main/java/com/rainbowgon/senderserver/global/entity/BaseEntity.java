fd4f70f5d0c2c847255c451e0fe271d5181aee16
전혜련 <davidpoplar@naver.com>
2023-11-10 13:57:27 +0900

    [fix] @PostConstruct 어노테이션 고려 코드 삭제

--- sender-server/src/main/java/com/rainbowgon/senderserver/global/entity/BaseEntity.java
+++ sender-server/src/main/java/com/rainbowgon/senderserver/global/entity/BaseEntity.java
@@ -5,7 +5,9 @@ import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @Getter
@@ -18,8 +20,4 @@ public class BaseEntity {
     @Column(updatable = false)
     private LocalDateTime createdAt;
 
-    @Enumerated(EnumType.STRING)
-    @ColumnDefault("'VALID'")
-    @Column(columnDefinition = "VARCHAR(10)")
-    private ValidStatus isValid = ValidStatus.VALID;
 }
