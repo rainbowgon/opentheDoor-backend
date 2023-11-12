903895727c249a370dbd02cde4aad85d5ad6603e
Lee, Nam Gon <nglee.dev@gmail.com>
2023-11-12 17:34:56 +0900

    [feat] ReservationController 생성

---
+++ reservation-service/src/main/java/com/rainbowgon/reservationservice/domain/reservation/controller/ReservationController.java
@@ -0,0 +1,11 @@
+package com.rainbowgon.reservationservice.domain.reservation.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/reservations")
+public class ReservationController {
+}
