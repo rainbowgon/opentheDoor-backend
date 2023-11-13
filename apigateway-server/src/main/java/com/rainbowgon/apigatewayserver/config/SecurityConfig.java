4643a66d77e6d52fb968bf33356297d073f355b3
최해미 <goal961@naver.com>
2023-11-13 10:42:03 +0900

    [fix] api gateway security config 수정

--- apigateway-server/src/main/java/com/rainbowgon/apigatewayserver/config/SecurityConfig.java
+++ apigateway-server/src/main/java/com/rainbowgon/apigatewayserver/config/SecurityConfig.java
@@ -1,17 +1,21 @@
 package com.rainbowgon.apigatewayserver.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
+@Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
+                .cors().disable()
                 .csrf().disable()
+                .httpBasic().disable()
                 .build();
     }
 }
