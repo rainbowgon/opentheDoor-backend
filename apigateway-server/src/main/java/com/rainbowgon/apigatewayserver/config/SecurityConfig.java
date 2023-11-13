01465dbf0650a72f9527eb2a1b5d08b15ed60fd1
Haemi Choi <86696821+chogoal@users.noreply.github.com>
2023-11-13 23:32:06 +0900

    [fix] api gateway security filter 수정 (#280)

--- apigateway-server/src/main/java/com/rainbowgon/apigatewayserver/config/SecurityConfig.java
+++ apigateway-server/src/main/java/com/rainbowgon/apigatewayserver/config/SecurityConfig.java
@@ -4,7 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.ServerSecurityContextRepository;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -12,10 +16,33 @@ public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
-        return http
+        http
+                .authorizeExchange()
+                .anyExchange().permitAll()
+                .and()
+                .securityContextRepository(new StatelessWebSessionSecurityContextRepository())
                 .cors().disable()
                 .csrf().disable()
                 .httpBasic().disable()
-                .build();
+                .headers()
+                .contentSecurityPolicy("script-src 'self'");
+
+        return http.build();
+    }
+
+    private static class StatelessWebSessionSecurityContextRepository implements ServerSecurityContextRepository {
+
+        private static final Mono<SecurityContext> EMPTY_CONTEXT = Mono.empty();
+
+        @Override
+        public Mono<Void> save(ServerWebExchange exchange, SecurityContext context) {
+            return Mono.empty();
+        }
+
+        @Override
+        public Mono<SecurityContext> load(ServerWebExchange exchange) {
+            return EMPTY_CONTEXT;
+        }
+
     }
 }
