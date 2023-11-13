2c772f11636da5e94b662103b60843798cd3a33c
전혜련 <davidpoplar@naver.com>
2023-11-13 11:16:42 +0900

    [fix] fcm 관련 코드 수정 후 테스트

---
+++ sender-server/src/main/java/com/rainbowgon/senderserver/domain/fcm/service/FCMConfig.java
@@ -0,0 +1,45 @@
+package com.rainbowgon.senderserver.domain.fcm.service;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+@Configuration
+public class FCMConfig {
+
+    @Value("${fcm.certification}")
+    private String credential;
+
+    @Bean
+    FirebaseMessaging firebaseMessaging() throws IOException {
+        ClassPathResource resource = new ClassPathResource(credential);
+        InputStream credentialStream = resource.getInputStream();
+
+        FirebaseApp firebaseApp = null;
+        List<FirebaseApp> firebaseAppList = FirebaseApp.getApps();
+
+        if (firebaseAppList != null && !firebaseAppList.isEmpty()) {
+            for (FirebaseApp app : firebaseAppList) {
+                if (app.getName().equals(FirebaseApp.DEFAULT_APP_NAME)) {
+                    firebaseApp = app;
+                }
+            }
+        } else {
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(credentialStream))
+                    .build();
+            firebaseApp = FirebaseApp.initializeApp(options);
+        }
+
+        return FirebaseMessaging.getInstance(firebaseApp);
+    }
+}
