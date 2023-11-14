4a7e60f6c388438669aff90effdec5e8e481bd58
Lee, Nam Gon <nglee.dev@gmail.com>
2023-11-14 17:06:21 +0900

    [chore] 형식 추가

--- reservation-service/src/main/java/com/rainbowgon/reservationservice/domain/reservation/dto/response/ReservationBriefResDto.java
+++ reservation-service/src/main/java/com/rainbowgon/reservationservice/domain/reservation/dto/response/ReservationBriefResDto.java
@@ -1,5 +1,6 @@
 package com.rainbowgon.reservationservice.domain.reservation.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -13,7 +14,9 @@ public class ReservationBriefResDto {
 
     private Long reservationId;
     private Long reservationNumber;
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate targetDate;
+    @JsonFormat(pattern = "hh:mm")
     private LocalTime targetTime;
     private Integer headcount;
     private Integer totalPrice;
