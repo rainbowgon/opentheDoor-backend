7e0b0af0806fb20c0850cc874e6faab6e8437dec
최해미 <goal961@naver.com>
2023-11-14 18:10:24 +0900

    [fix] feign 요청 GET -> POST 로 변경

--- member-service/src/main/java/com/rainbowgon/memberservice/global/client/SearchServiceClient.java
+++ member-service/src/main/java/com/rainbowgon/memberservice/global/client/SearchServiceClient.java
@@ -3,7 +3,7 @@ package com.rainbowgon.memberservice.global.client;
 import com.rainbowgon.memberservice.global.client.dto.input.ThemeDetailInDto;
 import com.rainbowgon.memberservice.global.client.dto.input.ThemeSimpleInDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
@@ -11,9 +11,9 @@ import java.util.List;
 @FeignClient(name = "search-service")
 public interface SearchServiceClient {
 
-    @GetMapping("/clients/themes/detail-lists")
+    @PostMapping("/clients/themes/detail-lists")
     List<ThemeDetailInDto> getBookmarkThemeDetailInfo(@RequestBody List<String> themeIdList);
 
-    @GetMapping("/clients/themes/simple-lists")
+    @PostMapping("/clients/themes/simple-lists")
     List<ThemeSimpleInDto> getBookmarkThemeSimpleInfo(@RequestBody List<String> themeIdList);
 }
