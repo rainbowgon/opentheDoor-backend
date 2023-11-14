33eb763dc50469347d24b67976b718e6abdba57b
Lee, Nam Gon <nglee.dev@gmail.com>
2023-11-14 14:40:04 +0900

    [env] ResponseWrapper 생성

---
+++ reservation-service/src/main/java/com/rainbowgon/reservationservice/global/response/PageInfo.java
@@ -0,0 +1,16 @@
+package com.rainbowgon.reservationservice.global.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+@AllArgsConstructor
+public class PageInfo {
+
+    private int page;
+    private int size;
+    private int totalElements;
+    private int totalPages;
+}
