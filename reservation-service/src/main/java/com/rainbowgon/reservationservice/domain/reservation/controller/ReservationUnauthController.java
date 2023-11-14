7623142fa49cddfa651ed0a7f355583090af23f5
Lee, Nam Gon <nglee.dev@gmail.com>
2023-11-14 16:41:16 +0900

    [feat] Controller 구현

--- reservation-service/src/main/java/com/rainbowgon/reservationservice/domain/reservation/controller/ReservationUnauthController.java
+++ reservation-service/src/main/java/com/rainbowgon/reservationservice/domain/reservation/controller/ReservationUnauthController.java
@@ -1,14 +1,16 @@
 package com.rainbowgon.reservationservice.domain.reservation.controller;
 
+import com.rainbowgon.reservationservice.domain.reservation.dto.request.ReservationReqDto;
 import com.rainbowgon.reservationservice.domain.reservation.dto.response.ReservationBaseInfoResDto;
+import com.rainbowgon.reservationservice.domain.reservation.dto.response.ReservationResultResDto;
+import com.rainbowgon.reservationservice.domain.reservation.dto.response.ReservationSuccess;
 import com.rainbowgon.reservationservice.domain.reservation.service.ReservationService;
 import com.rainbowgon.reservationservice.global.response.JsonResponse;
+import com.rainbowgon.reservationservice.global.response.ResponseWrapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,5 +26,19 @@ public class ReservationUnauthController {
                 reservationService.getReservationBaseInfo(themeId);
 
         return JsonResponse.ok("비회원의 테마 예약 페이지 정보를 가져왔습니다.", reservationBaseInfoResDto);
+    }
+
+    @PostMapping("/{theme-id}")
+    public ResponseEntity<ResponseWrapper<ReservationResultResDto>> makeReservation(
+            @RequestBody ReservationReqDto reservationReqDto) {
+
+        ReservationResultResDto reservationResultResDto =
+                reservationService.makeReservation(reservationReqDto);
+
+        if (reservationResultResDto.getIsSucceed().equals(ReservationSuccess.FAIL)) {
+            return JsonResponse.of(HttpStatus.CONFLICT, "예약에 실패했습니다.", reservationResultResDto);
+        }
+
+        return JsonResponse.ok("성공적으로 예약되었습니다.", reservationResultResDto);
     }
 }
