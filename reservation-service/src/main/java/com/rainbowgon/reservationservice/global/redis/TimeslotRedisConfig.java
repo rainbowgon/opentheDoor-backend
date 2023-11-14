9957295d37f0771309d8882ae84cd7f8535a54fc
Lee, Nam Gon <nglee.dev@gmail.com>
2023-11-15 05:31:31 +0900

    [env] TimeslotRedisConfig 추가

---
+++ reservation-service/src/main/java/com/rainbowgon/reservationservice/global/redis/TimeslotRedisConfig.java
@@ -0,0 +1,46 @@
+package com.rainbowgon.reservationservice.global.redis;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class TimeslotRedisConfig {
+
+    @Value("${spring.redis.timeslot.host}")
+    private String host;
+
+    @Value("${spring.redis.timeslot.port}")
+    private int port;
+
+    @Bean(name = "timeslotRedisConnectionFactory")
+    public RedisConnectionFactory timeslotRedisConnectionFactory() {
+
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPort(port);
+
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
+    @Bean(name = "timeslotRedisStringTemplate")
+    public RedisTemplate<String, String> timeslotRedisStringTemplate(
+            @Qualifier("timeslotRedisConnectionFactory")
+            RedisConnectionFactory timeslotRedisConnectionFactory) {
+
+        RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(timeslotRedisConnectionFactory);
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+
+        return redisTemplate;
+    }
+
+}
