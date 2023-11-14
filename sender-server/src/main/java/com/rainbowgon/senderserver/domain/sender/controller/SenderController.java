489f4c86278c9a60aa1c4431e9877f18e84b3bf9
전혜련 <davidpoplar@naver.com>
2023-11-14 13:39:33 +0900

    [fix] 작업 서버 통신 확인 불가

--- sender-server/src/main/java/com/rainbowgon/senderserver/domain/sender/controller/SenderController.java
+++ sender-server/src/main/java/com/rainbowgon/senderserver/domain/sender/controller/SenderController.java
@@ -2,7 +2,6 @@ package com.rainbowgon.senderserver.domain.sender.controller;
 
 import com.rainbowgon.senderserver.domain.fcm.service.FCMService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class SenderController {
 
     private final FCMService fcmService;
-
-    @GetMapping("/test")
-    public ResponseEntity<String> test() {
-
-        return ResponseEntity.ok("작업 서버 통신 테스트 성공");
-    }
 
     @GetMapping("/test/{token}")
     public String test(@PathVariable(name = "token") String token) {
