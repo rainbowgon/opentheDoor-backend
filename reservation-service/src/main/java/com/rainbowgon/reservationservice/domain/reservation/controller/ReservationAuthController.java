dd7b1c6cb52773002f21fd7d2bb1eed8cc8cd34a
Lee, Nam Gon <nglee.dev@gmail.com>
2023-11-14 14:41:36 +0900

    [fix] JsonResponse를 이용해서 반환

--- reservation-service/src/main/java/com/rainbowgon/reservationservice/domain/reservation/controller/ReservationAuthController.java
+++ reservation-service/src/main/java/com/rainbowgon/reservationservice/domain/reservation/controller/ReservationAuthController.java
@@ -2,6 +2,7 @@ package com.rainbowgon.reservationservice.domain.reservation.controller;
 
 import com.rainbowgon.reservationservice.domain.reservation.dto.response.ReservationBaseInfoResDto;
 import com.rainbowgon.reservationservice.domain.reservation.service.ReservationService;
+import com.rainbowgon.reservationservice.global.response.JsonResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +21,6 @@ public class ReservationAuthController {
         ReservationBaseInfoResDto reservationBaseInfoResDto =
                 reservationService.getReservationBaseInfo(memberId, themeId);
 
-        return ResponseEntity.ok(reservationBaseInfoResDto);
+        return JsonResponse.ok("회원의 테마 예약 페이지 정보를 가져왔습니다.", reservationBaseInfoResDto);
     }
 }
