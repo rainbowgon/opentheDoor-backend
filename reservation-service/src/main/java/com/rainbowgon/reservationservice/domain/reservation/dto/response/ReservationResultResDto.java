7c7778a8b229e138a44326fd02bd66df401e65d6
Lee, Nam Gon <nglee.dev@gmail.com>
2023-11-14 15:37:01 +0900

    [feat] 실패 성공 여부에 따라 생성되는 Dto 생성 메소드 생성

--- reservation-service/src/main/java/com/rainbowgon/reservationservice/domain/reservation/dto/response/ReservationResultResDto.java
+++ reservation-service/src/main/java/com/rainbowgon/reservationservice/domain/reservation/dto/response/ReservationResultResDto.java
@@ -11,4 +11,20 @@ public class ReservationResultResDto {
     private ReservationSuccess isSucceed;
     private Long reservationId;
     private Long reservationNumber;
+
+    public static ReservationResultResDto fail() {
+        return ReservationResultResDto.builder()
+                .isSucceed(ReservationSuccess.FAIL)
+                .reservationId(null)
+                .reservationNumber(null)
+                .build();
+    }
+
+    public static ReservationResultResDto success(Long reservationId, Long reservationNumber) {
+        return ReservationResultResDto.builder()
+                .isSucceed(ReservationSuccess.SUCCESS)
+                .reservationId(reservationId)
+                .reservationNumber(reservationNumber)
+                .build();
+    }
 }
