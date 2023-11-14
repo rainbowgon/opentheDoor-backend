d79091cb853ea3ea2a9b2a76de699187b68e58e0
Lee, Nam Gon <nglee.dev@gmail.com>
2023-11-14 15:29:18 +0900

    [feat] Exception 추가

---
+++ reservation-service/src/main/java/com/rainbowgon/reservationservice/global/error/exception/BookerInfoInvalidException.java
@@ -0,0 +1,13 @@
+package com.rainbowgon.reservationservice.global.error.exception;
+
+import com.rainbowgon.reservationservice.global.error.errorCode.GlobalErrorCode;
+
+
+public class BookerInfoInvalidException extends CustomException {
+
+    public static final CustomException EXCEPTION = new BookerInfoInvalidException();
+
+    public BookerInfoInvalidException() {
+        super(GlobalErrorCode.BOOKER_INFO_INVAILD);
+    }
+}
