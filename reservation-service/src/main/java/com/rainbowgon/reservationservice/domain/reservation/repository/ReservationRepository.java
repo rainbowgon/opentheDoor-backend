5b53097443ea3d88a022718f13885e4858b6bd30
Lee, Nam Gon <nglee.dev@gmail.com>
2023-11-14 17:36:25 +0900

    [feat] repository 메소드 추가

--- reservation-service/src/main/java/com/rainbowgon/reservationservice/domain/reservation/repository/ReservationRepository.java
+++ reservation-service/src/main/java/com/rainbowgon/reservationservice/domain/reservation/repository/ReservationRepository.java
@@ -4,8 +4,11 @@ import com.rainbowgon.reservationservice.domain.reservation.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 
     List<Reservation> findAllByMemberId(String memberId);
+
+    Optional<Reservation> findByMemberIdAndThemeId(String memberId, String themeId);
 }
