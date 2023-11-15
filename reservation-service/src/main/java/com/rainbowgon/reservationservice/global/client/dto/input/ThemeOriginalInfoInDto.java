db03a8d1312765bcb784d207db69476aa256af10
Lee, Nam Gon <nglee.dev@gmail.com>
2023-11-15 13:29:01 +0900

    [feat] 원본 테마 정보 요청 (#351)

---
+++ reservation-service/src/main/java/com/rainbowgon/reservationservice/global/client/dto/input/ThemeOriginalInfoInDto.java
@@ -0,0 +1,13 @@
+package com.rainbowgon.reservationservice.global.client.dto.input;
+
+import lombok.Getter;
+
+@Getter
+public class ThemeOriginalInfoInDto {
+
+    private String originalUrl;
+    private String originalPoster;
+    private String title;
+    private String venue;
+
+}
