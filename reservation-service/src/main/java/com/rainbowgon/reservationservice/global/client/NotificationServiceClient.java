676a025f17edb601fc5e6abb23d153e4d25d7a92
Lee, Nam Gon <nglee.dev@gmail.com>
2023-11-15 10:55:35 +0900

    [feat] 빈자리 알림 요청 구현

--- reservation-service/src/main/java/com/rainbowgon/reservationservice/global/client/NotificationServiceClient.java
+++ reservation-service/src/main/java/com/rainbowgon/reservationservice/global/client/NotificationServiceClient.java
@@ -1,6 +1,7 @@
 package com.rainbowgon.reservationservice.global.client;
 
 import com.rainbowgon.reservationservice.global.client.dto.output.SuccessNotificationOutDto;
+import com.rainbowgon.reservationservice.global.client.dto.output.WaitingNotificationOutDto;
 import com.rainbowgon.reservationservice.global.response.ResponseWrapper;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.lang.Nullable;
@@ -8,11 +9,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @FeignClient(name = "notification-service")
 @RequestMapping("/clients/notifications")
 public interface NotificationServiceClient {
 
     @PostMapping("/reservation")
     ResponseWrapper<Nullable> notifyReservationSuccess(
-            @RequestBody SuccessNotificationOutDto notificationOutDto);
+            @RequestBody SuccessNotificationOutDto successNotificationOutDto);
+
+    @PostMapping("/waiting")
+    ResponseWrapper<Nullable> notifyEmptyTheme(
+            @RequestBody List<WaitingNotificationOutDto> waitingNotificationOutDtoList);
 }
