89c31e392b6dd1a9728ef66bd43220d9bfa32c1d
Lee, Nam Gon <nglee.dev@gmail.com>
2023-11-15 09:32:35 +0900

    [fix] Primary 추가

--- reservation-service/src/main/java/com/rainbowgon/reservationservice/global/config/redis/TimeslotRedisConfig.java
+++ reservation-service/src/main/java/com/rainbowgon/reservationservice/global/config/redis/TimeslotRedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
@@ -19,6 +20,7 @@ public class TimeslotRedisConfig {
     @Value("${spring.redis.timeslot.port}")
     private int port;
 
+    @Primary
     @Bean(name = "timeslotRedisConnectionFactory")
     public RedisConnectionFactory timeslotRedisConnectionFactory() {
 
