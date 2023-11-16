8c30efca9522e1f362fa28a8a973558f0af224a4
Lee, Nam Gon <nglee.dev@gmail.com>
2023-11-16 10:14:40 +0900

    [fix] waitingId를 timeLineId를 활용해서 생성

--- reservation-service/src/main/java/com/rainbowgon/reservationservice/global/utils/RedisUtil.java
+++ reservation-service/src/main/java/com/rainbowgon/reservationservice/global/utils/RedisUtil.java
@@ -8,7 +8,8 @@ public class RedisUtil {
     // TODO 단순 String을 Hash로 저장
     public static String createWaitingId(String title, LocalDate targetDate,
                                          LocalTime targetTime) {
-        return String.format("%s-%s-%s", title, targetDate.toString(), targetTime.toString());
+        String timeLineId = createTimeLineId(title);
+        return String.format("%s-%s-%s", timeLineId, targetDate.toString(), targetTime.toString());
     }
 
     public static String createTimeLineId(String title) {
