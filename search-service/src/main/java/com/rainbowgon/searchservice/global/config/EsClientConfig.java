6c2707dadf0bd28c12f952604e8a50e1ae7aab4a
Byun Jung Won <101097111+Byungul@users.noreply.github.com>
2023-11-16 10:46:32 +0900

    [feat] Connection reset by peer 문제 해결을 위한 Timeout 설정 (#364)

--- search-service/src/main/java/com/rainbowgon/searchservice/global/config/EsClientConfig.java
+++ search-service/src/main/java/com/rainbowgon/searchservice/global/config/EsClientConfig.java
@@ -7,29 +7,29 @@ import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfigurat
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 import javax.validation.constraints.NotNull;
+import java.time.Duration;
 
 @Configuration
 @EnableElasticsearchRepositories
 public class EsClientConfig extends ElasticsearchConfiguration {
 
+    // 연결 타임아웃 및 소켓 타임아웃 설정을 위한 상수
+    private static final long CONNECTION_TIMEOUT = 10000; // 예: 10초
+    private static final long SOCKET_TIMEOUT = 60000; // 예: 60초
     @Value("${elasticsearch.host}")
     private String host;
-
     @Value("${elasticsearch.port}")
     private String port;
-
-//    @Value("${elasticsearch.username}")
-//    private String username;
-//
-//    @Value("${elasticsearch.password}")
-//    private String password;
 
     @NotNull
     @Override
     public ClientConfiguration clientConfiguration() {
         return ClientConfiguration.builder()
                 .connectedTo(host + ":" + port)
-//            .withBasicAuth(username, password)
+                // 연결 타임아웃과 소켓 타임아웃 설정
+                .withConnectTimeout(Duration.ofMillis(CONNECTION_TIMEOUT))
+                .withSocketTimeout(Duration.ofMillis(SOCKET_TIMEOUT))
+//                .withBasicAuth(username, password) // 필요한 경우 주석 해제
                 .build();
     }
 }
