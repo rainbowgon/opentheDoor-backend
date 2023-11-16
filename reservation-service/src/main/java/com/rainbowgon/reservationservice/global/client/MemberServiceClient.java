7caeb54beccf6e78ff0ea0b823b073a60f91e70e
Lee, Nam Gon <nglee.dev@gmail.com>
2023-11-16 10:59:15 +0900

    [feat] FcmToken 요청 방법 수정

--- reservation-service/src/main/java/com/rainbowgon/reservationservice/global/client/MemberServiceClient.java
+++ reservation-service/src/main/java/com/rainbowgon/reservationservice/global/client/MemberServiceClient.java
@@ -1,10 +1,15 @@
 package com.rainbowgon.reservationservice.global.client;
 
+import com.rainbowgon.reservationservice.global.client.dto.input.FcmTokenInDto;
 import com.rainbowgon.reservationservice.global.client.dto.input.MemberBriefInfoInDto;
+import com.rainbowgon.reservationservice.global.client.dto.output.MemberIdOutDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @FeignClient(name = "member-service")
 @RequestMapping("/clients/members")
@@ -13,6 +18,9 @@ public interface MemberServiceClient {
     @GetMapping("/booker/{member-id}")
     MemberBriefInfoInDto getMemberBriefInfo(@PathVariable("member-id") String memberId);
 
-    @GetMapping("/fcm/{member-id}")
-    String getFcmToken(@PathVariable("member-id") String memberId);
+    @GetMapping("/fcm")
+    FcmTokenInDto getFcmToken(@RequestBody MemberIdOutDto memberIdOutDto);
+
+    @GetMapping("/fcm/list")
+    List<FcmTokenInDto> getFcmTokenList(@RequestBody List<MemberIdOutDto> memberIdOutDtoList);
 }
