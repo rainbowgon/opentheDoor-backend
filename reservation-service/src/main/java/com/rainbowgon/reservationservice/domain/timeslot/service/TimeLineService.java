c27107b456106f75280c040ef81a97e7e021f90f
Lee, Nam Gon <nglee.dev@gmail.com>
2023-11-16 09:41:46 +0900

    [feat] TimeLineService에 메소드 추가

--- reservation-service/src/main/java/com/rainbowgon/reservationservice/domain/timeslot/service/TimeLineService.java
+++ reservation-service/src/main/java/com/rainbowgon/reservationservice/domain/timeslot/service/TimeLineService.java
@@ -1,4 +1,10 @@
 package com.rainbowgon.reservationservice.domain.timeslot.service;
 
+import com.rainbowgon.reservationservice.global.vo.TimeSlotVO;
+
+import java.util.List;
+
 public interface TimeLineService {
+
+    List<TimeSlotVO> getThemeTimeLine(String themeId);
 }
