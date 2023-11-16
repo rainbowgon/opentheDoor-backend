e1931a91181e5c94d747220691259b003e0bccb1
전혜련 <davidpoplar@naver.com>
2023-11-16 13:08:05 +0900

    [fix] 코드 오류 수정

--- notification-service/src/main/java/com/rainbowgon/notificationservice/domain/kafka/service/KafkaProducer.java
+++ notification-service/src/main/java/com/rainbowgon/notificationservice/domain/kafka/service/KafkaProducer.java
@@ -12,8 +12,7 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public cl
-        ass KafkaProducer {
+public class KafkaProducer {
 
     private final String TOPIC = "noti";
     private final KafkaTemplate<String, String> kafkaTemplate;
