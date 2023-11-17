71e8c679158de31bf73fda75af28d1a98fd34185
Lee, Nam Gon <nglee.dev@gmail.com>
2023-11-17 16:18:35 +0900

    [fix] 내역 반환 시에 title 입력 수정 (#415)

--- reservation-service/src/main/java/com/rainbowgon/reservationservice/domain/reservation/dto/response/ReservationDetailResDto.java
+++ reservation-service/src/main/java/com/rainbowgon/reservationservice/domain/reservation/dto/response/ReservationDetailResDto.java
@@ -32,8 +32,8 @@ public class ReservationDetailResDto {
     private String venue;
     private String location;
     private List<String> genre;
-    private String siteToS;
-    private String venueToS;
+    private String siteTos;
+    private String venueTos;
 
     public static ReservationDetailResDto from(Reservation reservation, ThemeBriefInfoInDto themeDto) {
         return ReservationDetailResDto.builder()
@@ -45,12 +45,12 @@ public class ReservationDetailResDto {
                 .totalPrice(reservation.getTotalPrice())
                 .themeId(reservation.getThemeId())
                 .poster(themeDto.getPoster())
-                .themeId(themeDto.getTitle())
+                .title(themeDto.getTitle())
                 .venue(themeDto.getVenue())
                 .location(themeDto.getLocation())
                 .genre(themeDto.getGenre())
-                .siteToS(themeDto.getSiteToS())
-                .venueToS(themeDto.getVenueToS())
+                .siteTos(themeDto.getSiteTos())
+                .venueTos(themeDto.getVenueTos())
                 .build();
     }
 }
