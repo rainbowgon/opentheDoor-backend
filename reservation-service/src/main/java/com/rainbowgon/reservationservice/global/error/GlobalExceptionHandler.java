2b418afc2e37e121471c597f5bfacabce3deecb0
Lee, Nam Gon <nglee.dev@gmail.com>
2023-11-17 15:08:09 +0900

    [feat] FeignException 처리

--- reservation-service/src/main/java/com/rainbowgon/reservationservice/global/error/GlobalExceptionHandler.java
+++ reservation-service/src/main/java/com/rainbowgon/reservationservice/global/error/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.rainbowgon.reservationservice.global.error;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rainbowgon.reservationservice.global.error.dto.ErrorReason;
 import com.rainbowgon.reservationservice.global.error.dto.ErrorResponse;
 import com.rainbowgon.reservationservice.global.error.errorCode.BaseErrorCode;
 import com.rainbowgon.reservationservice.global.error.errorCode.GlobalErrorCode;
 import com.rainbowgon.reservationservice.global.error.exception.CustomException;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +16,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 @RestControllerAdvice
 @Slf4j
 @RequiredArgsConstructor
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    private final ObjectMapper objectMapper;
 
     @ExceptionHandler(CustomException.class) // Business Exception
     public ResponseEntity<ErrorResponse> handleBusinessException(CustomException e,
@@ -26,6 +32,17 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         ErrorReason errorReason = code.getErrorReason();
         return ResponseEntity.status(errorReason.getStatus())
                 .body(ErrorResponse.of(errorReason, request.getRequestURL().toString()));
+    }
+
+    @ExceptionHandler(FeignException.class)
+    public ResponseEntity<Map> feignExceptionHandler(FeignException feignException) throws JsonProcessingException {
+
+        String responseJson = feignException.contentUTF8();
+        Map responseMap = objectMapper.readValue(responseJson, Map.class);
+
+        return ResponseEntity
+                .status(feignException.status())
+                .body(responseMap);
     }
 
     @ExceptionHandler(Exception.class)
