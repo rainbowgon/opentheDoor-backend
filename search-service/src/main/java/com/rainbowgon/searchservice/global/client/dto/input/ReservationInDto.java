1d3b44bdbe27a8e07dd672feffcc9abceb4d5894
변정원 <labhg64@gmail.com>
2023-11-19 11:40:25 +0900

    [fix] 예약을 위한 Dto 수정

--- search-service/src/main/java/com/rainbowgon/searchservice/global/client/dto/input/ReservationInDto.java
+++ search-service/src/main/java/com/rainbowgon/searchservice/global/client/dto/input/ReservationInDto.java
@@ -1,16 +1,14 @@
 package com.rainbowgon.searchservice.global.client.dto.input;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.rainbowgon.searchservice.global.client.dto.input.entry.TimeEntry;
 import lombok.Getter;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Getter
 public class ReservationInDto {
 
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    private LocalDate date;
+    //    @JsonFormat(pattern = "yyyy-MM-dd")
+//    private LocalDate date;
     private List<TimeEntry> timeList;
 }
