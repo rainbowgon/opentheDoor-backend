32f20fdde88e5d541f0f9d35bbff850ad49fda2f
Lee, Nam Gon <nglee.dev@gmail.com>
2023-11-19 23:07:54 +0900

    [chore] 로그 출력용 ToString 삭제

--- reservation-service/src/main/java/com/rainbowgon/reservationservice/global/utils/dto/ReservingServerResponseDto.java
+++ reservation-service/src/main/java/com/rainbowgon/reservationservice/global/utils/dto/ReservingServerResponseDto.java
@@ -1,10 +1,8 @@
 package com.rainbowgon.reservationservice.global.utils.dto;
 
 import lombok.Getter;
-import lombok.ToString;
 
 @Getter
-@ToString
 public class ReservingServerResponseDto {
 
     Boolean isSucceed;
