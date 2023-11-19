da7f971a06c0891410d9d16d9d105d6f8559202d
Lee, Nam Gon <nglee.dev@gmail.com>
2023-11-19 19:35:28 +0900

    [fix] 예약 서버와의 통신 수정 (#454)

--- reservation-service/src/main/java/com/rainbowgon/reservationservice/global/utils/dto/ReservingServerResponseDto.java
+++ reservation-service/src/main/java/com/rainbowgon/reservationservice/global/utils/dto/ReservingServerResponseDto.java
@@ -1,8 +1,10 @@
 package com.rainbowgon.reservationservice.global.utils.dto;
 
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
 public class ReservingServerResponseDto {
 
     Boolean isSucceed;
