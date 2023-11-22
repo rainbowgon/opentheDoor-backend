b68256c8912f7f241dc6689f3daea7842da310d1
Lee, Nam Gon <nglee.dev@gmail.com>
2023-11-22 11:02:02 +0900

    [feat] 테스트시에 설정된 값 반환

--- reservation-service/src/main/java/com/rainbowgon/reservationservice/global/connection/MasterkeyReservationAction.java
+++ reservation-service/src/main/java/com/rainbowgon/reservationservice/global/connection/MasterkeyReservationAction.java
@@ -16,10 +16,20 @@ public class MasterkeyReservationAction implements ReservationAction {
     private final ConnectionConfig config;
     private final RestTemplate restTemplate;
 
+    /**
+     * @return 실패인 경우, false 반환
+     * @return 성공인 경우, true 반환
+     */
     @Override
     public Boolean reserve(ReservingServerRequestDto reservingServerRequestDto) {
-        if (config.getOnOff().equalsIgnoreCase("OFF")) {
-            return false;
+        /*
+          config 파일에서 `on-off` 값이 `ON`인 경우에 실제 예약 동작함
+          그 외는 실제 동작을 하지 않음
+          `off-return-value`가 `SUCCESS`인 경우에 성공 테스트
+          `SUCCESS`가 아닌 그 외인 경우 실패 테스트
+         */
+        if (!config.getOnOff().equalsIgnoreCase("ON")) {
+            return config.getOffReturnValue().equalsIgnoreCase("SUCCESS");
         }
 
         String uri = getRequestUrl();
