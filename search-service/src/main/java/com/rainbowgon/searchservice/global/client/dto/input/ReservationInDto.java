28ce7fc22b6e40e4162676cdb8d833ee327e3a38
Byun Jung Won <101097111+Byungul@users.noreply.github.com>
2023-11-22 09:10:26 +0900

    [fix] Time 타입을 String 타입으로 수정 (#470)

--- search-service/src/main/java/com/rainbowgon/searchservice/global/client/dto/input/ReservationInDto.java
+++ search-service/src/main/java/com/rainbowgon/searchservice/global/client/dto/input/ReservationInDto.java
@@ -1,16 +1,13 @@
 package com.rainbowgon.searchservice.global.client.dto.input;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.rainbowgon.searchservice.global.client.dto.input.entry.TimeEntry;
 import lombok.Getter;
 
-import java.time.LocalDate;
 import java.util.List;
 
 @Getter
 public class ReservationInDto {
 
-    @JsonFormat(pattern = "yyyy-MM-dd")
-    private LocalDate date;
+    private String date;
     private List<TimeEntry> timeList;
 }
